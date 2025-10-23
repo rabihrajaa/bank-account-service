fe9f03f067d08729fc9527866a135eb2a304aba6
rabihrajaa <rajaarabihh@gmail.com>
2025-10-23 20:06:29 +0100

    first commit

---
+++ src/main/java/rabih/bankaccountservice/entities/BankAccount.java
@@ -0,0 +1,26 @@
+package rabih.bankaccountservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity
+@Data
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private Double balance;
+    private String currency;
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+    @ManyToOne
+    private Customer customer;
+
+
+}
---
+++ src/main/java/rabih/bankaccountservice/enums/AccountType.java
@@ -0,0 +1,5 @@
+package rabih.bankaccountservice.enums;
+
+public enum AccountType {
+    CURRENT_ACCOUNT, SAVINGS_ACCOUNT
+}
