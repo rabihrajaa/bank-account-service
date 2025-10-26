300b11a3e6dfe6e4a97b3001da414134f59afd82
rabihrajaa <rajaarabihh@gmail.com>
2025-10-26 16:15:16 +0100

    graphql1

--- src/main/java/rabih/bankaccountservice/web/BankAccountGraphqlController.java
+++ src/main/java/rabih/bankaccountservice/web/BankAccountGraphqlController.java
@@ -1,4 +1,22 @@
 package rabih.bankaccountservice.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import rabih.bankaccountservice.dtos.BankAccountResponseDTO;
+import rabih.bankaccountservice.services.BankAccountService;
+
+import java.util.List;
+
+@Controller
 public class BankAccountGraphqlController {
+    @Autowired
+    BankAccountService bankAccountService;
+
+    @QueryMapping
+    public List<BankAccountResponseDTO> getAllBankAccount(){
+        return bankAccountService.getAllBankAccounts();
+    }
+
 }
